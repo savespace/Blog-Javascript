b55a66f914839c9a6c1a980253c4b314b08c9197
Krzysztof Zielonka <zielonka.kontakt@outlook.com>
2025-08-14 07:08:02 +0200

    correct npx eslint, delete space

--- js/script.js
+++ js/script.js
@@ -1,61 +1,61 @@
 'use strict';
 
 const optArticleSelector = '.post',
-      optTitleSelector = '.post-title',
-      optTitleListSelector = '.titles';
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
 function titleClickHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const clickedElement = this;
-    console.log('Link was clicked!', clickedElement);
+  const clickedElement = this;
+  console.log('Link was clicked!', clickedElement);
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
-    for (const activeLink of activeLinks) {
-        activeLink.classList.remove('active');
-    }
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for (const activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
 
-    clickedElement.classList.add('active');
+  clickedElement.classList.add('active');
 
-    const allArticles = document.querySelectorAll('.posts article');
-    for (let article of allArticles) {
-        article.classList.remove('active');
-    }
+  const allArticles = document.querySelectorAll('.posts article');
+  for (let article of allArticles) {
+    article.classList.remove('active');
+  }
 
-    const articleSelector = clickedElement.getAttribute('href');
-    const targetArticle = document.querySelector(articleSelector);
-    if (targetArticle) {
-        targetArticle.classList.add('active');
-    }
+  const articleSelector = clickedElement.getAttribute('href');
+  const targetArticle = document.querySelector(articleSelector);
+  if (targetArticle) {
+    targetArticle.classList.add('active');
+  }
 }
 
 function generateTitleLinks() {
-     /* remove contents of titleList */
-     /* ... */
+  /* remove contents of titleList */
+  /* ... */
 
-    /* find all the articles and save them to variable: articles */
-    /* ... */
+  /* find all the articles and save them to variable: articles */
+  /* ... */
 
-    const titleList = document.querySelector(optTitleListSelector);
-    titleList.innerHTML = '';
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
 
-    const articles = document.querySelectorAll(optArticleSelector);
-    let html = '';
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
 
-    for (let article of articles) {
-        const articleId = article.getAttribute('id');
-        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+  for (let article of articles) {
+    const articleId = article.getAttribute('id');
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-        html += linkHTML;
-    }
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    html += linkHTML;
+  }
 
-    titleList.innerHTML = html;
+  titleList.innerHTML = html;
 
-    const links = document.querySelectorAll('.titles a');
-    for (const link of links) {
-        link.addEventListener('click', titleClickHandler);
-    }
+  const links = document.querySelectorAll('.titles a');
+  for (const link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
 }
 
 generateTitleLinks();
